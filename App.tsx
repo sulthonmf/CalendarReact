6841e16d0ca575c1badd704503380481e9462ae8
Sulthon Maulana Fathuddin <sulthon.maulana@photon.com>
2023-12-11 19:48:44 +0700

    remove unused code

--- App.tsx
+++ App.tsx
@@ -7,28 +7,12 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  useColorScheme,
-} from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 import NavigationStack from './src/navigation/NavigationStack';
 import {Provider} from 'react-redux';
 import { store } from './src/app/store';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <Provider store={store}>
